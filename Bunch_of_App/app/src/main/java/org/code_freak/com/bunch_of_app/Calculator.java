5a5bb26ad214b9d1ffeb0d6cfb656928f260c423
KuldiepZala <kuldiepzala@gmail.com>
2016-03-22 14:02:25 +0530

    this is json
    Signed-off-by: KuldiepZala <kuldiepzala@gmail.com>

--- Bunch_of_App/app/src/main/java/org/code_freak/com/bunch_of_app/Calculator.java
+++ Bunch_of_App/app/src/main/java/org/code_freak/com/bunch_of_app/Calculator.java
@@ -1,13 +1,16 @@
 package org.code_freak.com.bunch_of_app;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Calculator extends AppCompatActivity {
+import java.util.Locale;
 
+public class Calculator extends AppCompatActivity implements TextToSpeech.OnInitListener{
+    private TextToSpeech mTts;
     public void Calculation(View view){
         EditText etFirstNum = (EditText) findViewById(R.id.etFirstNum);
         EditText etSecondNum = (EditText) findViewById(R.id.etSecondNum);
@@ -39,15 +42,27 @@ public class Calculator extends AppCompatActivity {
         switch (view.getId()){
             case R.id.btnAdd:
                 tvResult.setText("addition is :"+(num1 + num2));
+                mTts.speak("Result is " + tvResult.getText(),
+                        TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                        null);
                 break;
             case R.id.btnMul:
                 tvResult.setText("mul is :"+(num1*num2));
+                mTts.speak("Result is " + tvResult.getText(),
+                        TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                        null);
                 break;
             case R.id.btnSub:
                 tvResult.setText("sub is :"+(num1-num2));
+                mTts.speak("Result is " + tvResult.getText(),
+                        TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                        null);
                 break;
             case R.id.btnDiv:
                 tvResult.setText("div is :"+(num1/num2));
+                mTts.speak("Result is " + tvResult.getText(),
+                        TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                        null);
                 break;
 
         }
@@ -56,6 +71,31 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator_layout);
+        mTts = new TextToSpeech(this, this);
+    }
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown!
+        if (mTts != null) {
+            mTts.stop();
+            mTts.shutdown();
+        }
+
+        super.onDestroy();
+    }
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Lanuage data is missing or the language is not supported.
+  //              Log.e(NavigatorActivity.TAG, "Language is not available.");
+            }
+        } else {
+            // Initialization failed.
+//            Log.e(NavigatorActivity.TAG, "Could not initialize TextToSpeech.");
+        }
 
     }
 }
--- Bunch_of_App/app/src/main/java/org/code_freak/com/bunch_of_app/Greet_User.java
+++ Bunch_of_App/app/src/main/java/org/code_freak/com/bunch_of_app/Greet_User.java
@@ -1,21 +1,63 @@
 package org.code_freak.com.bunch_of_app;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Greet_User extends AppCompatActivity {
-    public void GreetUser(View view){
-        EditText etName = (EditText) findViewById(R.id.etName);
-        TextView tvResult = (TextView) findViewById(R.id.tvResult);
-        tvResult.setText("Namste "+etName.getText());
+import java.util.Locale;
 
-    }
+public class Greet_User extends AppCompatActivity implements TextToSpeech.OnInitListener{
+    private TextToSpeech mTts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("Kuldeep", "onCreate() of GreetUserActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greet__user_layout);
+        mTts = new TextToSpeech(this, this);
     }
+
+    // when button is clicked this method will be called
+    public void GreetUser(View view){
+        Log.d("Kuldeep", "button onclick greetUser() of GreetUserActivity");
+        EditText etName = (EditText) findViewById(R.id.etName);
+        TextView tvResult = (TextView) findViewById(R.id.tvResult);
+        if(etName.getText().length() == 0) {
+            tvResult.setText("");
+            return;
+        }
+        tvResult.setText(" Namaste, " + etName.getText());
+        mTts.speak("Namaste "+etName.getText(),
+                TextToSpeech.QUEUE_FLUSH,  // Drop all pending entries in the playback queue.
+                null);
+    }
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown!
+        if (mTts != null) {
+            mTts.stop();
+            mTts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTts.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Lanuage data is missing or the language is not supported.
+                Log.e("Kuldeep", "Language is not available.");
+            }
+        } else {
+            // Initialization failed.
+            Log.e("Kuldeep", "Could not initialize TextToSpeech.");
+        }
+    }
+
 }
